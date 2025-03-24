a5b816c7a33b7f04c89f29f757e208af658ebf03
Karolina Banaszewska <kbanaszewskaa@gmail.com>
2025-03-24 13:08:21 +0100

    12. Brak nieuzywanych setterow

--- src/main/java/edu/kis/vh/nursery/FifoRhymer.java
+++ src/main/java/edu/kis/vh/nursery/FifoRhymer.java
@@ -4,6 +4,7 @@ public class FifoRhymer extends DefaultCountingOutRhymer {
 
     private final DefaultCountingOutRhymer temp = new DefaultCountingOutRhymer();
 
+
     @Override
     public int countOut() {
         while (!callCheck())
